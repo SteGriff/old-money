c30ad9119d73c171532ec1a48b2abb793ed3a8bd
Ste Griffiths <stephen.griffiths@nsg.com>
2021-05-28 17:32:43 +0100

    stuff

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export const add = (a: number, b: number): number => a + b + 1;
+export default class lds {
+	
+	_pence : number = 0;
+	
+	static readonly SHILLINGS_PER_POUND : number = 20;
+	static readonly PENCE_PER_SHILLING : number = 12;
+	static readonly PENCE_PER_POUND : number = 240;
+	
+	get pounds() { return parseInt(this._pence / PENCE_PER_POUND); }
+	get shillings() { return parseInt(this._pence / PENCE_PER_SHILLING) % SHILLINGS_PER_POUND; }
+	get pence() { return this._pence % PENCE_PER_SHILLING; }
+	
+	constructor(pence : number = 0) {
+		this._pence = pence;
+	}
+}
