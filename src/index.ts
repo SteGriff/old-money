4e9f8457b69549dc7274b6be601b499f8a525509
Ste Griffiths <github@stegriff.co.uk>
2021-06-01 16:34:54 +0100

    Making it work

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-export default class lds {
+class lds {
 	
 	_pence : number = 0;
 	
-	static readonly SHILLINGS_PER_POUND : number = 20;
-	static readonly PENCE_PER_SHILLING : number = 12;
-	static readonly PENCE_PER_POUND : number = 240;
+	readonly SHILLINGS_PER_POUND : number = 20;
+	readonly PENCE_PER_SHILLING : number = 12;
+	readonly PENCE_PER_POUND : number = 240;
 	
-	get pounds() { return parseInt(this._pence / PENCE_PER_POUND); }
-	get shillings() { return parseInt(this._pence / PENCE_PER_SHILLING) % SHILLINGS_PER_POUND; }
-	get pence() { return this._pence % PENCE_PER_SHILLING; }
+	get pounds() { return parseInt(this._pence / this.PENCE_PER_POUND); }
+	get shillings() { return parseInt(this._pence / this.PENCE_PER_SHILLING) % this.SHILLINGS_PER_POUND; }
+	get pence() { return this._pence % this.PENCE_PER_SHILLING; }
 	
 	constructor(pence : number = 0) {
 		this._pence = pence;
 	}
 }
+
+export { lds };
--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,6 @@
-const test = require('ava');
+import test from 'ava'
+import { lds } from '../dist/index.js'
+
 
 test('foo', t => {
 	t.pass();
@@ -7,4 +9,28 @@ test('foo', t => {
 test('bar', async t => {
 	const bar = Promise.resolve('bar');
 	t.is(await bar, 'bar');
+});
+
+test('lds class is available', t => {
+	let m = new lds();
+	t.truthy(m);
+});
+
+test('lds instantiates with a pence amount', t => {
+	let m = new lds(1);
+	t.is(m.pence, 1);
+});
+
+test('lds 12p -> 1s', t => {
+	let m = new lds(12);
+	t.is(m.pounds, 0);
+	t.is(m.shillings, 1);
+	t.is(m.pence, 0);
+});
+
+test('lds 240p -> £1', t => {
+	let m = new lds(240);
+	t.is(m.pounds, 1);
+	t.is(m.shillings, 0);
+	t.is(m.pence, 0);
 });
